3186db8847f6a6fd89024ab860af32579284a09e
PranavSKK <113448123+PranavSKK@users.noreply.github.com>
2022-10-04 18:11:32 +0530

    My portfolio Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*-----About Tabs-----*/
+
+// const tabsContainer = document.querySelector(".about-tabs"),
+//     aboutSection = document.querySelector(".about-section");
+
+// tabsContainer.addEventListener("click",(e)=>{
+//     if(e.target.classList.contains("tab-item") && e.target.classList.contains())
+// });
+
+/*----Portfolio Item Detail Popup----*/
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("view-project-btn")){
+        console.log("hi")
+    }
+})
